d9ccdcfe3f632b16d832f4970bdf6bc1300f1e5e
Jack Mcleans <jackagwaya@gmail.com>
2020-01-24 17:33:33 +0300

    Sending post requests in python to TUK_DATA API

---
+++ POST_REQUEST/post.py
@@ -0,0 +1,49 @@
+import requests as request
+import random
+from time import sleep
+
+
+
+
+def generate_temperature():
+    temperature = random.randrange(21,30,1)
+    return temperature
+
+def generate_humidity():
+    humidity = random.randrange(20,100,3)
+    return humidity
+
+def estimate_airQuality():
+    temp = generate_temperature()
+    humidity = generate_humidity()
+    if(temp > 60 and humidity < 40):
+        airQuality = "Harsh"
+    else:
+        airQuality = "Fair"
+
+    return airQuality
+
+
+def post(url,json_data):
+    real_time_endpoint = "/real-time"
+    real_time_url = url+real_time_endpoint
+    response = request.post(url,json = json_data)
+    real_time_response = request.post(real_time_url,json = json_data)
+    return response,json_data
+
+
+
+
+while(True):
+    post_data = {
+    "temperature":generate_temperature(),
+    "humidity":generate_humidity(),
+    "airQuality":estimate_airQuality()
+}
+    
+    response,data = post(post_url,post_data)
+    print(response)
+    print(data)
+    print("------------------------")
+    
+    sleep(2)
